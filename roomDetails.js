a7e1080f9d69cff9e627e98b429d268c37f22bdc
Vash117 <88488046+Vash117@users.noreply.github.com>
2021-12-14 10:34:17 +0200

    Update roomDetails.js

--- roomDetails.js
+++ roomDetails.js
@@ -6,7 +6,7 @@ import {
   curentFloorTemplate,
 } from "./createFloorsView.js";
 
-const modaltemplate = (room, floorNumber) => html`
+const modaltemplate = ( room, floorNumber) => html`
   <div class="container-fluid modal-holder">
     <div class="details-page">
       <button
@@ -58,11 +58,19 @@ const modaltemplate = (room, floorNumber) => html`
 `;
 
 export function displayModal(e, id, floorNumber) {
-  const curentRoom = floors[floorNumber].rooms.find(
-    (room) => room.roomNumber == id
-  );
-  let result = modaltemplate(curentRoom, floorNumber);
-  render(result, document.querySelector("main"));
+  try{
+    const curentRoom = floors[floorNumber].rooms.find(
+      (room) => room.roomNumber == id
+      );
+    if(!curentRoom.obj){
+      throw new Error('Room is free!!')
+    }
+
+    let result = modaltemplate(curentRoom, floorNumber);
+    render(result, document.querySelector("main"));
+  }catch(err){
+    alert(err.message)
+  }
 }
 function closeModal(curentFloor) {
   let result = curentFloorTemplate(floors[curentFloor]);
